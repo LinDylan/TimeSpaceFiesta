e9176c96f28b1d13a37e1da40a63dec2dbb9978b
Dylanl19 <dylanl19109051@tas.tw>
2017-11-20 09:06:19 +0800

    Initial Project
    2D Jump and Left Right Movement

---
+++ Assets/Player/Scripts/Player_Gravity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Gravity : MonoBehaviour {
+	Rigidbody2D rb2d = null;
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rb2d.velocity.y < 0) {
+			rb2d.velocity += Vector2.up * Physics2D.gravity.y * (2.5f - 1) * Time.deltaTime;
+		} else if (rb2d.velocity.y > 0 && !Input.GetKeyDown(KeyCode.Space)) {
+			rb2d.velocity += Vector2.up * Physics2D.gravity.y * (4 - 1) * Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Player/Scripts/Player_Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+	float speed = 5.0f;
+	Rigidbody2D rb2d = null;
+	bool jumping;
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		jumping = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey (KeyCode.A)) {
+			rb2d.velocity = new Vector2 (-1 * 100 * speed * Time.deltaTime, rb2d.velocity.y);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			rb2d.velocity = new Vector2 (1 * 100 * speed * Time.deltaTime, rb2d.velocity.y);
+		}
+		if (Input.GetKeyUp (KeyCode.D) || Input.GetKeyUp (KeyCode.A)) {
+			rb2d.velocity = new Vector2 (0, rb2d.velocity.y);
+		}
+		if (Input.GetKeyDown(KeyCode.Space) && !jumping) {
+			rb2d.velocity = new Vector2(rb2d.velocity.x, 17.5f);
+			jumping = true;
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "Land") {
+			jumping = false;
+		}
+	}
+}
